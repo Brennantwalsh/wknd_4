50ff5b3b90268b2712f903be08e523b9c7212724
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-09-17 17:39:53 -0500

    moduling

--- oop/calc_2.rb
+++ oop/calc_2.rb
@@ -3,7 +3,7 @@
 # a different technique using Composition. Hint: Google 'Ruby Mixin Module'. (Using
 # mixin modules is how you achieve Composition with Ruby.)
 
-class SimpleCalculator
+module SimpleCalculator
 
   def add(first_number, second_number)
     first_number + second_number
@@ -24,28 +24,28 @@ class SimpleCalculator
 end
 
 class FancyCalculator
-
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
+  include SimpleCalculator
 
   def square_root(number)
     Math.sqrt(number)
   end
-
 end
 
 # Copy your driver code from the previous exercise below:
-
+fcalc = FancyCalculator.new
+if fcalc.square_root(12) == 3.4641016151377544
+  puts "Success"
+else
+  puts "F"
+end
+puts fcalc.subtract(1, 2)
+if fcalc.subtract(1, 2) == -1
+  puts "Success"
+else
+  puts "F"
+end
+if fcalc.divide(3, 1) == 3
+  puts "Success"
+else
+  puts "f"
+end

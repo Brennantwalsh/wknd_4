e4b9f45de125283aa8c653d1c496ab265eea0840
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-09-17 14:58:15 -0500

    flatten and slice maybe nice but its probably easier than that

--- algo/haystack.rb
+++ algo/haystack.rb
@@ -3,3 +3,5 @@
 # haystack[1]
 
 haystack = {hay: [:hay, :hay, :hay, {hay: {hay: [:hay, {hay: [:hay, :hay, :needle]}, :hay, :hay, :hay]}}, :hay, :hay]}
+
+puts haystack.flatten.slice!(1).slice!(3).flatten.slice!(1).flatten.slice!(1).flatten.slice!(1).flatten.slice!(1)[2]
